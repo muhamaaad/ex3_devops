251912b088ff13d36b9073c865780a4614080d67
muhamad1235 <muhamad-18fadel@hotmail.com>
2025-11-23 00:45:57 +0200

    Updating The Hellow.java And The Muhamad.java Files To Check Jobs were Triggerd

--- Hellow.java
+++ Hellow.java
@@ -1,7 +1,7 @@
 public class Hellow{
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("Hello World ..."+i);
+            System.out.println("not Hello World ..."+i);
 }
 }
 } 
--- Muhamad.java
+++ Muhamad.java
@@ -1,7 +1,7 @@
 public class Muhamad{
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println(i+"...Egbaria Muhamad");
+            System.out.println(i+"...not Egbaria Muhamad");
 }
 }
 } 
